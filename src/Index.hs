868b7daf0940e63554fba38c6c5c3e718f743ec2
Tsubasa Namiki <namikingsoft@gmail.com>
2016-07-25 00:49:35 +0900

    feat: try to get messages of channel from slack api

--- src/Index.hs
+++ src/Index.hs
@@ -2,13 +2,16 @@ module Index
   ( execute
   ) where
 
+import Prelude hiding (id)
 import System.Environment
 import Control.Applicative
-import Text.Printf
-import Data.Aeson
 import Network.HTTP.Conduit
+import Text.Printf
+import Data.Maybe
+import Data.Aeson
 import Data.ByteString.Lazy.Char8 (unpack)
 
+import Slack.ChannelHistory
 import Slack.ChannelList
 import Slack.Channel
 
@@ -19,9 +22,15 @@ execute = do
     putStrLn ""
     putStrLn $ unpack json
     let channelList = parse json
-        channel = fromName channelList "general"
-    putStrLn ""
+        maybeChannel = fromName channelList "general"
+        channel = fromMaybe (error "Channel Not Found") maybeChannel
+        channelId = id channel
     print channel
+    json <- simpleHttp $ printf urlChannelHistory token channelId
+    putStrLn $ unpack json
+    let messages = channelHistoryMessages $ parseChannelHistory json
+    putStrLn ""
+    print messages
   where
     urlChannelList = "https://slack.com/api/channels.list?token=%s"
     urlChannelHistory = "https://slack.com/api/channels.history?token=%s&channel=%s"
