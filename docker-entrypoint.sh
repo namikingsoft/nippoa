f6993cd57f45f0f3e63342903b90831ffdfed6f7
Tsubasa Namiki <namikingsoft@gmail.com>
2016-08-01 01:13:57 +0900

    feat: set timezone on docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -eu
 
+# setup timezone
+if [ -n "${TIMEZONE-}" ]; then
+  cp "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
+fi
+
+# idle
 if [ "$1" = "idle" ]; then
   exec tail -f /dev/null
 fi
