ca8798737ca62ec7e2672d8a04c7c5f6c5c4e720
Tsubasa Namiki <namikingsoft@gmail.com>
2016-08-07 00:36:03 +0900

    test: add index

---
+++ test/IndexSpec.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE CPP #-}
+module IndexSpec where
+
+import Index()
+import Test.Hspec
+
+spec :: Spec
+spec = do
+
+  describe "execute" $ do
+    it "should execute nippoa" $ do
+      pending
