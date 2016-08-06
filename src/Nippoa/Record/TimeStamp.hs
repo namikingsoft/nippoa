a57fa67170ce0124b201498c904be3d01dd887eb
Tsubasa Namiki <namikingsoft@gmail.com>
2016-08-06 23:13:32 +0900

    feat: add timestamp of record

---
+++ src/Nippoa/Record/TimeStamp.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE CPP #-}
+module Nippoa.Record.TimeStamp
+  ( TimeStamp (..)
+  , timeStampFromTs
+  ) where
+
+import Data.Time.LocalTime
+  ( ZonedTime
+  , utcToZonedTime
+  , hoursToTimeZone
+  )
+import Data.Time.Format
+  ( parseTimeOrError
+  , defaultTimeLocale
+  )
+import Data.Time.Clock
+  ( UTCTime
+  )
+
+data TimeStamp = TimeStamp
+               { timeStampZonedTime :: ZonedTime
+               } deriving (Show)
+
+timeStampFromTs :: String -> TimeStamp
+timeStampFromTs = TimeStamp . utcToZonedTime jst . utcFromTs
+  where
+    utcFromTs x = parseTimeOrError True defaultTimeLocale "%s%Q" x :: UTCTime
+    jst = hoursToTimeZone 9
---
+++ test/Nippoa/Record/TimeStampSpec.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE CPP #-}
+module Nippoa.Record.TimeStampSpec where
+
+import Nippoa.Record.TimeStamp
+import Test.Hspec
+
+import Data.Time.LocalTime
+import Data.Time.Format
+import Data.Time.Clock
+
+zonedtime0 :: ZonedTime
+zonedtime0 = utcToZonedTime jst . utcFromTs $ "2016-07-29 14:01:18"
+  where
+    utcFromTs x = parseTimeOrError True defaultTimeLocale "%F %T" x :: UTCTime
+    jst = hoursToTimeZone 9
+
+timestamp0 :: TimeStamp
+timestamp0 = TimeStamp zonedtime0
+
+spec :: Spec
+spec = do
+
+  describe "timeStampZonedTime" $ do
+    it "should return initial value" $ do
+      show (timeStampZonedTime timestamp0) `shouldBe` show zonedtime0
+
+  describe "timeStampFromTs" $ do
+    it "should return new instance from timestamp of slack" $ do
+      let format = formatTime defaultTimeLocale "%F %T"
+          zonedtime = timeStampZonedTime . timeStampFromTs $ "1469768478.000747"
+      format zonedtime `shouldBe` "2016-07-29 14:01:18"
