467fefb7fa73999948c5b456a14ac6c67d6e0a98
Tsubasa Namiki <namikingsoft@gmail.com>
2016-08-12 00:10:08 +0900

    feat: add image48 on profile

--- src/Slack/Profile.hs
+++ src/Slack/Profile.hs
@@ -22,6 +22,7 @@ data Profile
   = Profile
   { profileRealName :: String
   , profileImage24 :: String
+  , profileImage48 :: String
   , profileBotId :: Maybe String
   } deriving (Show, Eq)
 
@@ -29,11 +30,13 @@ instance FromJSON Profile where
   parseJSON (Object v) = Profile
     <$> v .:  "real_name"
     <*> v .:  "image_24"
+    <*> v .:  "image_48"
     <*> v .:? "bot_id"
 
 instance ToJSON Profile where
-  toJSON (Profile name image24 botId) = object
+  toJSON (Profile name image24 image48 botId) = object
     [ "real_name" .= name
     , "image_24" .= image24
+    , "image_48" .= image48
     , "bot_id"   .= botId
     ]
--- test/Slack/ProfileSpec.hs
+++ test/Slack/ProfileSpec.hs
@@ -9,6 +9,7 @@ profile0
   = Profile
   { profileRealName = "realname0"
   , profileImage24 = "image240"
+  , profileImage48 = "image480"
   , profileBotId = Just "botid0"
   }
 
@@ -17,6 +18,7 @@ profile1
   = Profile
   { profileRealName = "realname1"
   , profileImage24 = "image241"
+  , profileImage48 = "image481"
   , profileBotId = Just "botid1"
   }
 
@@ -32,6 +34,11 @@ spec = do
     it "should return initial value" $ do
       profileImage24 profile0 `shouldBe` "image240"
       profileImage24 profile1 `shouldBe` "image241"
+
+  describe "profileImage48" $ do
+    it "should return initial value" $ do
+      profileImage48 profile0 `shouldBe` "image480"
+      profileImage48 profile1 `shouldBe` "image481"
 
   describe "profileBotId" $ do
     it "should return initial value" $ do
